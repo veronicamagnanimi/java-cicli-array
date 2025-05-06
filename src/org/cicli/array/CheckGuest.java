c1107cfbaf6a86a7274b5b2c331da8829003e922
veronicamagnanimi <veronica.magnanimi@hotmail.it>
2025-05-06 16:34:55 +0200

    feat: array + ciclo per stampare ogni elemento

---
+++ src/org/cicli/array/CheckGuest.java
@@ -0,0 +1,13 @@
+package org.cicli.array;
+
+public class CheckGuest {
+public static void main(String[] args) {
+
+    //array + ciclo dove stampo ogni elemento
+  String[] friends = {"Luca", "Valentina", "Sara", "Francesca", "Chiara", "Claudia"};
+   for(int i = 0; i < friends.length; i++) {
+    System.out.println(friends[i]);
+   }
+
+}
+}
